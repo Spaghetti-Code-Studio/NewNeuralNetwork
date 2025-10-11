87640af3fd507267967d2742dc1289675abe28ed
Marek Eibel <536376@mail.muni.cz>
2025-10-11 15:09:20 +0200

    fix: missing parameter in scripts
    Ninja on VS handles this correctly based on build type itself

--- scripts/build-production.sh
+++ scripts/build-production.sh
@@ -1,2 +1,2 @@
 [ -d build ] && echo "Build directory already exists, skipping mkdir." || mkdir build
-cd build && cmake -DCMAKE_BUILD_TYPE=Production .. && make
+cd build && cmake -DCMAKE_BUILD_TYPE=Production -DINCLUDE_TESTS=OFF .. && make
--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,2 +1,2 @@
 [ -d build ] && echo "Build directory already exists, skipping mkdir." || mkdir build
-cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
+cd build && cmake -DCMAKE_BUILD_TYPE=Release -DINCLUDE_TESTS=ON .. && make
