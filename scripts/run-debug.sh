bdd013ad40a041ee900a8f7e8aa3da81249845dd
Marek Eibel <536376@mail.muni.cz>
2025-10-11 14:51:58 +0200

    fix: script skip mkdir step if build directory already exists

--- scripts/run-debug.sh
+++ scripts/run-debug.sh
@@ -1,2 +1,2 @@
-mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./src/NewNeuralNetworkProgram
-
+[ -d build ] && echo "Build directory already exists, skipping mkdir." || mkdir build
+cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./src/NewNeuralNetworkProgram
--- scripts/run-release.sh
+++ scripts/run-release.sh
@@ -1,2 +1,2 @@
-mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make && ./src/NewNeuralNetworkProgram
-
+[ -d build ] && echo "Build directory already exists, skipping mkdir." || mkdir build
+cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make && ./src/NewNeuralNetworkProgram
