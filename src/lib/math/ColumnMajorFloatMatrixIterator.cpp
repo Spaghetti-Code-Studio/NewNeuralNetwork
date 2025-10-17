37bec938ff9ebbb8119c433c67257b1767d0b177
Jakubisek <jakubisek555@seznam.cz>
2025-10-17 11:47:06 +0200

    feat: remove redundant iter constructor

--- src/lib/math/ColumnMajorFloatMatrixIterator.cpp
+++ src/lib/math/ColumnMajorFloatMatrixIterator.cpp
@@ -2,13 +2,9 @@
 #include "FloatMatrix.hpp"
 
 namespace nnn {
-  ColumnMajorFloatMatrixIterator::ColumnMajorFloatMatrixIterator(const FloatMatrix* mat, size_t row, size_t col)
-      : m_matrix(mat), m_row(row), m_col(col) {}
-
   ColumnMajorFloatMatrixIterator::ColumnMajorFloatMatrixIterator(const FloatMatrix* mat)
       : m_matrix(mat), m_row(0), m_col(0) {}
 
-  // TODO: keep original starting row and col and set them here
   void ColumnMajorFloatMatrixIterator::Restart() {
     m_row = 0;
     m_col = 0;
