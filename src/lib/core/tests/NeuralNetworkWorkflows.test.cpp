da9a977581e774b9fdd68ba53fca26e3bad86a76
Marek Eibel <536376@mail.muni.cz>
2025-10-23 18:03:38 +0200

    feat: implement workflow tests

---
+++ src/lib/core/tests/NeuralNetworkWorkflows.test.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include <memory>
+
+#include "DenseLayer.hpp"
+#include "FloatMatrix.hpp"
+#include "ReLU.hpp"
+#include "NeuralNetwork.hpp"
+
+TEST_CASE("Neural Network Forward Pass Basic Dense Layer Forward Evaluation With ReLU") {
+  auto relu = nnn::ReLU();
+  auto input = nnn::FloatMatrix::Create(3, 1, {1.0f, 2.0f, 3.0f});
+
+  auto neuralNetwork = nnn::NeuralNetwork();
+  neuralNetwork.AddLayer(std::make_unique<nnn::DenseLayer>(3, 2, relu));
+  auto result = neuralNetwork.RunForwardPass(input.value());
+
+  CHECK_THAT(result(0, 0), Catch::Matchers::WithinAbs(4.820, 0.001));
+  CHECK_THAT(result(1, 0), Catch::Matchers::WithinAbs(3.986, 0.001));
+
+  auto biasResult = nnn::FloatMatrix::Create(2, 1, {-5.0f, 5.0f});
+  auto& bias = biasResult.value();
+
+  auto value = neuralNetwork.GetLayer(0);
+  nnn::ILayer* baseLayer = value;
+  nnn::DenseLayer* layer = dynamic_cast<nnn::DenseLayer*>(baseLayer);
+  layer->Update(layer->GetWeights(), bias);
+
+  result = neuralNetwork.RunForwardPass(input.value());
+
+  CHECK_THAT(result(0, 0), Catch::Matchers::WithinAbs(0.0f, 0.001));
+  CHECK_THAT(result(1, 0), Catch::Matchers::WithinAbs(8.986, 0.001));
+}
+
+// TEST_CASE("Basic DenseLayer Forward update") {
+//   auto layer = nnn::DenseLayer(3, 2);
+//   auto weights = nnn::FloatMatrix::Create(2, 3, {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f});
+//   auto biases = nnn::FloatMatrix::Ones(2, 1);
+//   layer.Update(weights.value(), biases);
+//
+//   auto inputResult = nnn::FloatMatrix::Create(3, 1, {1.0f, 2.0f, 3.0f});
+//   auto& input = inputResult.value();
+//   auto result = layer.Forward(input);
+//
+//   CHECK_THAT(result(0, 0), Catch::Matchers::WithinAbs(4.820, 0.001));
+//   CHECK_THAT(result(1, 0), Catch::Matchers::WithinAbs(3.986, 0.001));
+// }
