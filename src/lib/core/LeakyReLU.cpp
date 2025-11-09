54b60b83e23485b8c15f975a5320b8d4cfc1ba3f
Jakubisek <jakubisek555@seznam.cz>
2025-11-09 19:10:32 +0100

    feat: add leaky ReLU

---
+++ src/lib/core/LeakyReLU.cpp
@@ -0,0 +1,14 @@
+#include "FloatMatrix.hpp"
+#include "LeakyReLU.hpp"
+
+nnn::LeakyReLU::LeakyReLU(float alpha): m_alpha(alpha) {}
+
+void nnn::LeakyReLU::Evaluate(FloatMatrix& input) const {
+    const float alpha = m_alpha;
+    input.MapInPlace([alpha](float x) { return x > 0 ? x : x * alpha; });
+}
+
+void nnn::LeakyReLU::Derivative(FloatMatrix& input) const {
+    const float alpha = m_alpha;
+    input.MapInPlace([alpha](float x) { return x > 0 ? 1 : alpha; });
+}
