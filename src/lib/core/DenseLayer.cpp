9e662e4f720467d509c40eb065a2458d8326f9b6
Marek Eibel <536376@mail.muni.cz>
2025-11-13 18:11:26 +0100

    feat: implement stochastic gradient descent with backpropagation

--- src/lib/core/DenseLayer.cpp
+++ src/lib/core/DenseLayer.cpp
@@ -3,18 +3,54 @@
 
 namespace nnn {
 
+  DenseLayer::DenseLayer(
+      size_t batchSize, size_t inputSize, size_t outputSize, std::unique_ptr<IActivationFunction>&& activationFunction)
+      : m_inputSize(inputSize),
+        m_outputSize(outputSize),
+        m_biases(FloatMatrix::Zeroes(outputSize, 1)),
+        m_weights(FloatMatrix::Random(outputSize, inputSize)),
+        m_activationFunction(std::move(activationFunction)),
+        m_lastInnerPotential(FloatMatrix::Zeroes(outputSize, batchSize)),
+        m_lastInput(FloatMatrix::Zeroes(inputSize, batchSize)),
+        m_gradientWeigths(outputSize, inputSize),
+        m_gradientBias(outputSize, 1)
+
+  {}
+
   DenseLayer::DenseLayer(size_t inputSize, size_t outputSize, std::unique_ptr<IActivationFunction>&& activationFunction)
       : m_inputSize(inputSize),
         m_outputSize(outputSize),
         m_biases(FloatMatrix::Zeroes(outputSize, 1)),
         m_weights(FloatMatrix::Random(outputSize, inputSize)),
-        m_activationFunction(std::move(activationFunction)) {}
+        m_activationFunction(std::move(activationFunction)),
+        m_lastInnerPotential(FloatMatrix::Zeroes(outputSize, 1)),
+        m_lastInput(FloatMatrix::Zeroes(inputSize, 1)),
+        m_gradientWeigths(outputSize, inputSize),
+        m_gradientBias(outputSize, 1)
+
+  {}
 
   FloatMatrix DenseLayer::Forward(const FloatMatrix& inputVector) const {
     auto result = m_weights * inputVector;
     result.AddToAllCols(m_biases);
     m_activationFunction->Evaluate(result);
     return result;
+  }
+
+  FloatMatrix DenseLayer::Backward(const FloatMatrix& gradient) {
+    // slide 213
+    m_activationFunction->Derivative(m_lastInnerPotential);  // sigma'(inner potential)
+    auto hnc = m_lastInnerPotential.Hadamard(gradient);      // dE/dy * sigma'(inner potential)
+    m_lastInput.Transpose();
+    m_gradientWeigths = hnc * m_lastInput;  // dE/dw
+
+    m_gradientBias = FloatMatrix::SumColumns(hnc);
+
+    hnc.Transpose();
+    auto nextGradient = hnc * m_weights;  // dE/dy+1
+    nextGradient.Transpose();
+    return nextGradient;  // here the final dimensions are cols = batch, rows = input, where input is acutally same size
+                          // as output of next
   }
 
   void DenseLayer::Update(const FloatMatrix& weights, const FloatMatrix& biases) {
--- src/lib/core/DenseLayer.hpp
+++ src/lib/core/DenseLayer.hpp
@@ -10,12 +10,25 @@ namespace nnn {
 
   class DenseLayer : public ILayer {
    public:
+    DenseLayer(size_t batchSize,
+        size_t inputSize,
+        size_t outputSize,
+        std::unique_ptr<IActivationFunction>&& activationFunction);
+
     DenseLayer(size_t inputSize, size_t outputSize, std::unique_ptr<IActivationFunction>&& activationFunction);
     FloatMatrix Forward(const FloatMatrix& inputVector) const override;
+
+    /**
+     * @param gradient how much does the loss change when my outputs change (dE/dy)
+     * @returns how much the loss changes when next layer outputs change (with respect to backward pass)
+     */
+    FloatMatrix Backward(const FloatMatrix& gradient) override;
     void Update(const FloatMatrix& weights, const FloatMatrix& biases) override;
 
     const FloatMatrix& GetWeights() const override;
     const FloatMatrix& GetBiases() const override;
+    inline FloatMatrix GetGradientWeights() const { return m_gradientWeigths; }
+    inline FloatMatrix GetGradientBiases() const { return m_gradientBias; }
 
    private:
     size_t m_inputSize;
@@ -23,5 +36,9 @@ namespace nnn {
     FloatMatrix m_weights;
     FloatMatrix m_biases;
     std::unique_ptr<IActivationFunction> m_activationFunction;
+    FloatMatrix m_lastInnerPotential;
+    FloatMatrix m_lastInput;
+    FloatMatrix m_gradientWeigths;
+    FloatMatrix m_gradientBias;
   };
 }  // namespace nnn
