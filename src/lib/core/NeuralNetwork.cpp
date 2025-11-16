7c05cbc18ff573d8119621d730d46efba7ec3c62
Marek Eibel <536376@mail.muni.cz>
2025-11-16 22:48:50 +0100

    feat: brekaing api changes (WIP)
    differ between hidden and output layers, passes are done using ForEachLayer iterator methods

--- src/lib/core/NeuralNetwork.cpp
+++ src/lib/core/NeuralNetwork.cpp
@@ -1,45 +1,62 @@
 #include "NeuralNetwork.hpp"
-#include "DenseLayer.hpp"
 
 namespace nnn {
 
-  size_t NeuralNetwork::AddLayer(std::unique_ptr<ILayer>&& layer) {
-    m_layers.push_back(std::move(layer));
-    return m_layers.size() - 1;
+  size_t NeuralNetwork::AddHiddenLayer(std::unique_ptr<ILayer>&& layer) {
+    m_hiddenLayers.push_back(std::move(layer));
+    return m_hiddenLayers.size() - 1;
   }
 
-  FloatMatrix NeuralNetwork::RunForwardPass(FloatMatrix input) {  //
+  size_t NeuralNetwork::SetOutputLayer(std::unique_ptr<IOutputLayer>&& layer) {
+    m_outputLayer = std::move(layer);
+    return m_hiddenLayers.size();
+  }
 
-    for (const auto& layer : m_layers) {
-      input = layer->Forward(input);
-    }
-
+  FloatMatrix NeuralNetwork::RunForwardPass(FloatMatrix input) {
+    ForEachLayer([&](ILayer& layer) { input = layer.Forward(input); });
     return input;
   }
 
-  void NeuralNetwork::Train(const FloatMatrix& input, const FloatMatrix& expected, HyperParameters params) {
+  // TODO: implement this
+  FloatMatrix NeuralNetwork::RunBackwardPass(FloatMatrix input) {
+    return FloatMatrix::Identity(4);  // tmp
+  }
+
+  void NeuralNetwork::Train(const FloatMatrix& input, const FloatMatrix& expected, HyperParameters params) {  //
+
     auto actual = RunForwardPass(input);
 
-    // TODO: compute correct loss function
-    FloatMatrix loss = (actual - expected) * 2.0f;
+    FloatMatrix gradient = m_outputLayer->ComputeOutputGradient(actual, expected);
 
-    for (int i = m_layers.size() - 1; i >= 0; --i) {
-      loss = m_layers[i]->Backward(loss);
+    // TODO: use backward ForEachLayer here
+    gradient = m_outputLayer->Backward(gradient);
+    for (int i = (int)m_hiddenLayers.size() - 1; i >= 0; --i) {
+      gradient = m_hiddenLayers[i]->Backward(gradient);
     }
 
-    for (const auto& layer : m_layers) {
-      FloatMatrix newWeights = layer->GetWeights() - (layer->GetWightsGradient() * params.learningRate);
-      FloatMatrix newBiases = layer->GetBiases() - (layer->GetBiasesGradient() * params.learningRate);
-      layer->Update(newWeights, newBiases);
+    // TODO: use backward ForEachLayer here
+    {
+      FloatMatrix newWeights = m_outputLayer->GetWeights() - (m_outputLayer->GetWightsGradient() * params.learningRate);
+      FloatMatrix newBiases = m_outputLayer->GetBiases() - (m_outputLayer->GetBiasesGradient() * params.learningRate);
+      m_outputLayer->Update(newWeights, newBiases);
     }
+    ForEachLayer([&](ILayer& layer) {
+      FloatMatrix newWeights = layer.GetWeights() - (layer.GetWightsGradient() * params.learningRate);
+      FloatMatrix newBiases = layer.GetBiases() - (layer.GetBiasesGradient() * params.learningRate);
+      layer.Update(newWeights, newBiases);
+    });
   }
 
   ILayer* NeuralNetwork::GetLayer(size_t index) {  //
 
-    if (index >= m_layers.size()) {
+    if (index > m_hiddenLayers.size()) {
       return nullptr;
     }
 
-    return m_layers[index].get();
+    if (index == m_hiddenLayers.size()) {
+      return m_outputLayer.get();
+    }
+
+    return m_hiddenLayers[index].get();
   }
 }  // namespace nnn
--- src/lib/core/NeuralNetwork.hpp
+++ src/lib/core/NeuralNetwork.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
+#include <utility>
+#include <functional>
 #include <memory>
 #include <vector>
 
 #include "FloatMatrix.hpp"
 #include "ILayer.hpp"
+#include "IOutputLayer.hpp"
 
 namespace nnn {
+  /**
+   * @warning This API is not safe yet, for example, user is not anyhow forced to set output layer leading to null
+   * pointer exception.
+   */
   class NeuralNetwork {
    public:
     struct HyperParameters {
@@ -15,13 +22,45 @@ namespace nnn {
 
     NeuralNetwork() = default;
 
-    size_t AddLayer(std::unique_ptr<ILayer>&& layer);
+    size_t AddHiddenLayer(std::unique_ptr<ILayer>&& layer);
+    size_t SetOutputLayer(std::unique_ptr<IOutputLayer>&& layer);
     ILayer* GetLayer(size_t index);
 
     FloatMatrix RunForwardPass(FloatMatrix input);
+
+    // TODO: implement this
+    FloatMatrix RunBackwardPass(FloatMatrix input);
+
+    // TODO: this will take epochs number and will do the automatic training on batches (we must take whole training
+    // dataset here)
     void Train(const FloatMatrix& input, const FloatMatrix& expected, HyperParameters params);
 
-   private:
-    std::vector<std::unique_ptr<ILayer>> m_layers;
+   protected:
+    std::unique_ptr<IOutputLayer> m_outputLayer;
+    std::vector<std::unique_ptr<ILayer>> m_hiddenLayers;
+
+    virtual void ForEachLayerImpl(const std::function<void(ILayer&)>& func) {
+      for (auto& layer : m_hiddenLayers) {
+        func(*layer);
+      }
+      func(*m_outputLayer);
+    }
+
+    virtual void ForEachLayerImpl(const std::function<void(const ILayer&)>& func) const {
+      for (const auto& layer : m_hiddenLayers) {
+        func(*layer);
+      }
+      func(*m_outputLayer);
+    }
+
+    template <typename Function>
+    void ForEachLayer(Function&& func) {
+      ForEachLayerImpl(std::function<void(ILayer&)>(std::forward<Function>(func)));
+    }
+
+    template <typename Function>
+    void ForEachLayer(Function&& func) const {
+      ForEachLayerImpl(std::function<void(const ILayer&)>(std::forward<Function>(func)));
+    }
   };
 }  // namespace nnn
