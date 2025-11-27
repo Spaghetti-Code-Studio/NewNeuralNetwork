aedf6304e5809aeb97f0dde25ad9666ad1ab41c8
Marek Eibel <536376@mail.muni.cz>
2025-11-27 18:20:53 +0100

    feat: add some prints
    wip

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,20 +5,19 @@
 #include <CSVReader.hpp>
 #include <DataLoader.hpp>
 #include <DenseLayer.hpp>
-#include <FloatMatrix.hpp>
 #include <LeakyReLU.hpp>
 #include <NeuralNetwork.hpp>
 #include <NormalGlorotWeightInitializer.hpp>
+#include <NormalHeWeightInitializer.hpp>
 #include <SoftmaxDenseOutputLayer.hpp>
 #include <TestDataSoftmaxEvaluator.hpp>
 #include <Timer.hpp>
-#include <NormalHeWeightInitializer.hpp>
 
 int main(int argc, char* argv[]) {  //
 
-  std::cout << "Starting" << std::endl;
-
-  nnn::Timer timer;
+  std::cout << "NewNeuralNetwork\n"
+            << "Training neural network on MNIST fashion dataset.\n"
+            << std::endl;
 
   int seed = 42;
   auto glorotInit = nnn::NormalGlorotWeightInitializer(seed);
@@ -32,7 +31,9 @@ int main(int argc, char* argv[]) {  //
       std::make_unique<nnn::DenseLayer>(128, 54, std::make_unique<nnn::LeakyReLU>(0.05f), heInit));
   size_t l3 = neuralNetwork.SetOutputLayer(std::make_unique<nnn::SoftmaxDenseOutputLayer>(54, 10, glorotInit));
 
-  std::cout << "Loading data..." << std::endl;
+  nnn::Timer timer;
+  timer.Start();
+  std::cout << "Loading dataset..." << std::endl;
   auto reader = std::make_shared<nnn::CSVReader>();
   auto datasetResult = nnn::DataLoader::Load({.trainingFeatures = "../../../../data/fashion_mnist_train_vectors.csv",
                                                  .trainingLabels = "../../../../data/fashion_mnist_train_labels.csv",
@@ -44,17 +45,20 @@ int main(int argc, char* argv[]) {  //
   if (datasetResult.has_error()) {
     std::cout << datasetResult.error() << std::endl;
     return -1;
+  } else {
+    std::cout << "Loading of dataset took " << timer.End() << " seconds.\n" << std::endl;
   }
 
-  std::cout << "Training data..." << std::endl;
+  std::cout << "Training neural network..." << std::endl;
   timer.Start();
-  auto statistics = neuralNetwork.Train(datasetResult.value().trainingDataset);
-  std::cout << "Training time: " << timer.End() << " seconds.\n" << std::endl;
+  auto dataset = datasetResult.value();
+  auto statistics = neuralNetwork.Train(dataset.trainingDataset);
+  std::cout << "Training took " << timer.End() << " seconds." << std::endl;
   statistics.Print();
 
-  std::cout << "Evaluation..." << std::endl;
-  auto result = neuralNetwork.RunForwardPass(*datasetResult.value().testingFeatures);
-  auto evaluation = nnn::TestDataSoftmaxEvaluator::Evaluate(result, *datasetResult.value().testingLabels);
+  std::cout << "\nEvaluation of neural network on testing data..." << std::endl;
+  auto result = neuralNetwork.RunForwardPass(*dataset.testingFeatures);
+  auto evaluation = nnn::TestDataSoftmaxEvaluator::Evaluate(result, *dataset.testingLabels);
   evaluation.Print();
 
   return 0;
